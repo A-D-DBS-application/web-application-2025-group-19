68448446022d96f438179c551cd85cc25000a3dd
PierreBroekaert <pierre.broekaert@ugent.be>
2025-11-24 14:06:34 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,21 @@
 # app/__init__.py
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 from .models import db
 
 migrate = Migrate()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates")
     app.config.from_object(Config)
 
+    # Initialize extensions
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # Register blueprints
     with app.app_context():
         from .routes import main
         app.register_blueprint(main)
