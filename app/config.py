b67d651839b9aae464d113aa11118e9667269559
PierreBroekaert <pierre.broekaert@ugent.be>
2025-11-24 14:07:26 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,18 @@
-class Config: 
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:password@localhost:port/dbname'
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "devkey123")
+
+    # Supabase database gegevens
+    SUPABASE_USER = "postgres"
+    SUPABASE_PASSWORD = "sleep_inn"   # <-- jouw wachtwoord
+    SUPABASE_HOST = "db.wwhwdffacdoyriqmrphs.supabase.co"  # <-- jouw Supabase host
+    SUPABASE_DB = "postgres"
+
+    # Connection string voor SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{SUPABASE_USER}:{SUPABASE_PASSWORD}"
+        f"@{SUPABASE_HOST}:5432/{SUPABASE_DB}"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
